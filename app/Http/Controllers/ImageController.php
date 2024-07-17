e6d27f2ba02f939da9ec0289c8a32e8638271102
manikandan-1704 <manimk1704@gmail.com>
2024-07-17 21:46:04 +0530

    Checking dev branch

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -9,19 +9,19 @@ use Intervention\Image\Facades\Image;
 class ImageController extends Controller
 {
     public function upload(Request $request)
-{
-    $request->validate([
-        'image' => 'required|image', 
-    ]);
+    {
+        $request->validate([
+            'image' => 'required|image',
+        ]);
 
-    $file = $request->file('image');
-    $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-    $extension = $file->getClientOriginalExtension();
-    $filename = $originalFilename . '_' . time() . '.' . $extension;
-    $imagePath = $file->storeAs('uploads', $filename, 'public');
+        $file = $request->file('image');
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $extension = $file->getClientOriginalExtension();
+        $filename = $originalFilename . '_' . time() . '.' . $extension;
+        $imagePath = $file->storeAs('uploads', $filename, 'public');
 
-    return response()->json(['image_path' => $imagePath]);
-}
+        return response()->json(['image_path' => $imagePath]);
+    }
 
 
     public function convert(Request $request)
@@ -45,15 +45,13 @@ class ImageController extends Controller
     }
 
     public function download($filename)
-{
-    $path = storage_path('app/public/converted/' . $filename);
+    {
+        $path = storage_path('app/public/converted/' . $filename);
 
-    if (!Storage::exists('public/converted/' . $filename)) {
-        abort(404);
+        if (!Storage::exists('public/converted/' . $filename)) {
+            abort(404);
+        }
+
+        return response()->download($path);
     }
-
-    return response()->download($path);
 }
-
-}
-
